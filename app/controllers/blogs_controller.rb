cc4f7484f77197b744616149cada5aeabee0a171
Seiya-wasabi <se.sato614@gmail.com>
2021-04-07 02:00:16 +0000

    [対エラ〜]

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,16 +1,17 @@
 class BlogsController < ApplicationController
   def index
+    
   end
 
   def show
   end
 
   def new
-    @blog =Blog.new
+    @blog = Blog.new
   end
   
   def create
-    blog = Blog.new(blog_params)
+    @blog = Blog.new(blog_params)
     blog.save
     redirect_to blogs_path
   end
